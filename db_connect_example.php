2a46fbc547642e01839ef085ac514290c8b80793
ThanGrove <than@virginia.edu>
2014-02-02 10:29:54 -0500

    Updating db_connect sample data to use descriptive faux information

--- db_connect_example.php
+++ db_connect_example.php
@@ -1,10 +1,8 @@
 <?php
 
 define("HOST", "localhost"); // The host you want to connect to.
-//define("USER", "tsec_user"); // The database username.
-//define("PWD", "jkw32Hs0WlO97Vbsq"); // The database password. 
-define("USER", "root"); // The database username.
-define("PWD", "r00t"); // The database password. 
+define("USER", "##PUT USER NAME HERE##"); // The database username.
+define("PWD", "##PUT PASSWORD HERE##"); // The database password. 
 define("DATABASE", "tarotdata"); // The database name.
 
 $mysqli = new mysqli(HOST, USER, PWD, DATABASE);
